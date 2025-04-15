cf044e05d44fd3c9407d31784584890d0e287a9a
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-15 21:28:13 +0500

    up

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
 //     { name: "banana", price: 10 }
 // ]
 // console.log(sortDrinks(drinks));
-
+//------------------------------------------------------------------------
 // TASK - 2 Convert Key, Values in an Object to Array
 // function toArrey(object="") {
 //     arr1 = []
@@ -29,7 +29,7 @@
 //     C: 3
 // }
 // console.log(toArrey(objectToArray));
-
+//------------------------------------------------------------------------
 // TASK - 3 Destructuring Objects X
 // let names = []
 // let users = [
@@ -45,7 +45,7 @@
 // 2 - usul
 // const name2 = users.map(users => users.name)
 // console.log(name2);
-
+//------------------------------------------------------------------------
 // TASK - 4 Ageing the Population...
 // function func1(obj) {
 //     return function (num) {
@@ -63,3 +63,63 @@
 //     "Elderly": 71
 // }
 // console.log(func1(afterNYears)(2));
+//------------------------------------------------------------------------
+// TASK - 5 CRUD DATABASE Simulation application
+// function create(user) {
+//     ism="Saloh"
+//     yosh = "33"
+//     user.studentsList[ism] = {
+//         name: ism,
+//         age: yosh,
+//         id: Math.floor(Math.random() * 200) + 1,
+//         tolov: true
+//     }
+//     return user
+// }
+
+// function read(name,user) {
+//     if (name in user.studentsList) {
+//         return user.studentsList[name]
+//     } else {
+//         return "Bunday ismli odam yoq"
+//     }
+// }
+
+// function update(name="", user="") {
+//     if (!name in user.studentsList) {
+//         return "Bunday ismli odam yoq"
+//     }
+//     user.studentsList[name] = {
+//         name: "Jasurbek",
+//         age: 27,
+//         tolov: false,
+//         id: Math.floor(Math.random() * 200) + 1
+//     }
+//     return user
+// }
+// function delete1(name, user) {
+//     if (user.studentsList && user.studentsList[name]) {
+//         delete user.studentsList[name]
+//         console.log(`${name} o'chirildi.`);
+//     } else {
+//         console.log(`${name} topilmadi.`);
+//     }
+// }
+
+// const database = {
+//     studentsList: {
+//         'Xamidullo': {
+//             name: "Xamidullo",
+//             age: 33,
+//             id: 123,
+//             tolov: false
+//         }
+//     },
+// }
+// // console.log(create(database));
+// // console.log(read("Saloh", database));
+// // console.log(update("Saloh", database));
+// // console.log(new delete1("Xamidullo",database));
+// // console.log(database);
+//------------------------------------------------------------------------
+// TASK - 6  Repeating Letters
--- js/test.js
+++ js/test.js
@@ -1,18 +1,60 @@
-// TASK - 4 Ageing the Population...
-// function func1(obj) {
-//     return function (num) {
-//         for (let key in obj) {
-//             obj[key] += num;
-//         }
-//         return obj
+// TASK - 5 CRUD DATABASE Simulation application
+// function create(user) {
+//     ism="Saloh"
+//     yosh = "33"
+//     user.studentsList[ism] = {
+//         name: ism,
+//         age: yosh,
+//         id: Math.floor(Math.random() * 200) + 1,
+//         tolov: true
+//     }
+//     return user
+// }
+
+// function read(name,user) {
+//     if (name in user.studentsList) {
+//         return user.studentsList[name]
+//     } else {
+//         return "Bunday ismli odam yoq"
 //     }
 // }
-// afterNYears = {
-//     "Baby": 2,
-//     "Child": 8,
-//     "Teenager": 15,
-//     "Adult": 25,
-//     "Elderly": 71
+
+// function update(name="", user="") {
+//     if (!name in user.studentsList) {
+//         return "Bunday ismli odam yoq"
+//     }
+//     user.studentsList[name] = {
+//         name: "Jasurbek",
+//         age: 27,
+//         tolov: false,
+//         id: Math.floor(Math.random() * 200) + 1
+//     }
+//     return user
 // }
-// console.log(func1(afterNYears)(2));
+// function delete1(name, user) {
+//     if (user.studentsList && user.studentsList[name]) {
+//         delete user.studentsList[name]
+//         console.log(`${name} o'chirildi.`);
+//     } else {
+//         console.log(`${name} topilmadi.`);
+//     }
+// }
+
+// const database = {
+//     studentsList: {
+//         'Xamidullo': {
+//             name: "Xamidullo",
+//             age: 33,
+//             id: 123,
+//             tolov: false
+//         }
+//     },
+// }
+// // console.log(create(database));
+// // console.log(read("Saloh", database));
+// // console.log(update("Saloh", database));
+// // console.log(new delete1("Xamidullo",database));
+// // console.log(database);
+
+
 

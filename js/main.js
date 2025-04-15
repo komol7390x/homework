65634bcc4829c97588059de9ec80c300638f2fe5
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-15 19:53:42 +0500

    up

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
-console.log("Komol");
+// HOME WORK
+// TASK - 1 Drink Sorting
 
+// function sortDrinks(arr) {
+//     return arr.sort((a,b)=>a.price-b.price)
+// }
+
+// drinks = [
+//     { name: "lemonade", price: 50 },
+//     { name: "lime", price: 30 },
+//     { name: "alcohol", price: 20 },
+//     { name: "apple", price: 60 },
+//     { name: "banana", price: 10 }
+// ]
+// console.log(sortDrinks(drinks));
+
+// TASK - 2 Convert Key, Values in an Object to Array
+// function toArrey(object="") {
+//     arr1 = []
+//     let len=object.length
+//     for (let key in object) {
+//         arr1.push([key,object[key]])
+//     }
+//     return arr1
+// }
+// objectToArray={
+//     D: 1,
+//     B: 2,
+//     C: 3
+// }
+// console.log(toArrey(objectToArray));
---
+++ js/test.js
@@ -0,0 +1,15 @@
+// TASK - 2 Obyekt ichidagi kalit (key) va qiymatlarni (value) massivga aylantirish
+// function toArrey(object="") {
+//     arr1 = []
+//     let len=object.length
+//     for (let key in object) {
+//         arr1.push([key,object[key]])
+//     }
+//     return arr1
+// }
+// objectToArray={
+//     D: 1,
+//     B: 2,
+//     C: 3
+// }
+// console.log(toArrey(objectToArray));

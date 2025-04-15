9f6a47a372fe58e7dc255471135de9aa2ad0a737
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-15 20:13:08 +0500

    up

--- js/main.js
+++ js/main.js
@@ -29,3 +29,19 @@
 //     C: 3
 // }
 // console.log(toArrey(objectToArray));
+
+// TASK - 3 Destructuring Objects X
+// let names = []
+// let users = [
+//     { name: "John", email: "john@example.com" },
+//     { name: "Jason", email: "jason@example.com" },
+//     { name: "Jeremy", email: "jeremy@example.com" },
+//     { name: "Jacob", email: "jacob@example.com" }
+// ]
+// for (let key in users) {
+//     names.push(users[key].name)
+// }
+// console.log(names);
+// 2 - usul
+// const name2 = users.map(users => users.name)
+// console.log(name2);
--- js/test.js
+++ js/test.js
@@ -1,15 +1,15 @@
-// TASK - 2 Obyekt ichidagi kalit (key) va qiymatlarni (value) massivga aylantirish
-// function toArrey(object="") {
-//     arr1 = []
-//     let len=object.length
-//     for (let key in object) {
-//         arr1.push([key,object[key]])
-//     }
-//     return arr1
+// TASK - 3 Destructuring Objects X
+// let names = []
+// let users = [
+//     { name: "John", email: "john@example.com" },
+//     { name: "Jason", email: "jason@example.com" },
+//     { name: "Jeremy", email: "jeremy@example.com" },
+//     { name: "Jacob", email: "jacob@example.com" }
+// ]
+// for (let key in users) {
+//     names.push(users[key].name)
 // }
-// objectToArray={
-//     D: 1,
-//     B: 2,
-//     C: 3
-// }
-// console.log(toArrey(objectToArray));
+// console.log(names);
+// 2 - usul
+// const name2 = users.map(users => users.name)
+// console.log(name2);

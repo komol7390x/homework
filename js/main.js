d5dc083d3e97282f1841c72bcaf9c6a562fab9c0
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-16 09:55:27 +0500

    up

--- js/main.js
+++ js/main.js
@@ -235,7 +235,7 @@
 //------------------------------------------------------------------------
 // TASK - 9 Add Strings
 // var addStrings = function (num1, num2) {
-//     res = String(parseFloat(num1) + parseFloat(num2))
+//     res = String(BigInt(num1) + BigInt(num2))
 //     return res
 // };
 // console.log(addStrings("11", "123"));

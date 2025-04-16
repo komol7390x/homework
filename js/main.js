133b62be3a5620095b819ab6959060748260fd0e
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-16 09:38:19 +0500

    up

--- js/main.js
+++ js/main.js
@@ -122,6 +122,62 @@
 // // console.log(new delete1("Xamidullo",database));
 // // console.log(database);
 //------------------------------------------------------------------------
+// TASK - 5 CRUD DATABASE Simulation application
+let dataBase = {
+//     students: [],
+//     read() {
+//         console.log(this.students);
+//     },
+//     create(user) {
+//         let find = this.students.filter((val) => val.name == user.name);
+//         if (find.length) {
+//             console.log(`Bu ${user.name} avval qo'shilgan`);
+//             return 0
+//         }
+//         this.students.push(user)
+//         console.log(`${user.name} muaffaqiyatli qo'shildi`);
+//     },
+//     update(name, user) {
+//         let findIndex = this.students.findIndex((val) => val.name == name);
+//         if (findIndex == -1) {
+//             console.log(`${name} bunday foydalanuvchi topilmadi`);
+//             return 0;
+//         }
+//         this.students[findIndex] = {
+//             ...this.students[findIndex],
+//             ...user
+//         };
+//         console.log(`${name} ma'lumot yangilandi`);
+//     },
+//     findOne(name) {
+//         findName = this.students.findIndex((val) => val.name == name)
+//         if (findName !== -1) {
+//             console.log(`${name} topildi`);
+//             return 0
+//         }
+//         console.log(`${name} bunday user yo'q`);
+//     },
+//     delete(name) {
+//         findName = this.students.findIndex((val) => val.name == name)
+//         if (findName === -1) {
+//             console.log(`${name} topilmadi`);
+//             return 0
+//         }
+//         this.students = this.students.filter((val) => val.name != name);
+//         console.log("User o'chirildi");
+//     },
+// }
+
+// dataBase.create({ name: "Xamidullo", age: 33, id: 123, tolov: false })
+// dataBase.create({ name: "Alisher", age: 27, id: 123, tolov: false })
+// dataBase.read()
+// dataBase.update("Alisher", { age: 27, id: 999, tolov: true })
+// dataBase.read()
+// dataBase.findOne("Alisher1")
+// dataBase.delete("Xamidullo")
+// dataBase.read()
+
+//------------------------------------------------------------------------
 // TASK - 6 Repeating Letters
 // function double(str = "") {
 //     if (typeof str!=='string') {
--- js/test.js
+++ js/test.js
@@ -1,7 +1,54 @@
-// TASK - 9 Add Strings
-// var addStrings = function (num1, num2) {
-//     res = String(parseFloat(num1) + parseFloat(num2))
-//     return res
-// };
-// console.log(addStrings("11", "123"));
+// let dataBase = {
+//     students: [],
+//     read() {
+//         console.log(this.students);
+//     },
+//     create(user) {
+//         let find = this.students.filter((val) => val.name == user.name);
+//         if (find.length) {
+//             console.log(`Bu ${user.name} avval qo'shilgan`);
+//             return 0
+//         }
+//         this.students.push(user)
+//         console.log(`${user.name} muaffaqiyatli qo'shildi`);
+//     },
+//     update(name, user) {
+//         let findIndex = this.students.findIndex((val) => val.name == name);
+//         if (findIndex == -1) {
+//             console.log(`${name} bunday foydalanuvchi topilmadi`);
+//             return 0;
+//         }
+//         this.students[findIndex] = {
+//             ...this.students[findIndex],
+//             ...user
+//         };
+//         console.log(`${name} ma'lumot yangilandi`);
+//     },
+//     findOne(name) {
+//         findName = this.students.findIndex((val) => val.name == name)
+//         if (findName !== -1) {
+//             console.log(`${name} topildi`);
+//             return 0
+//         }
+//         console.log(`${name} bunday user yo'q`);
+//     },
+//     delete(name) {
+//         findName = this.students.findIndex((val) => val.name == name)
+//         if (findName === -1) {
+//             console.log(`${name} topilmadi`);
+//             return 0
+//         }
+//         this.students = this.students.filter((val) => val.name != name);
+//         console.log("User o'chirildi");
+//     },
+// }
+
+// dataBase.create({ name: "Xamidullo", age: 33, id: 123, tolov: false })
+// dataBase.create({ name: "Alisher", age: 27, id: 123, tolov: false })
+// dataBase.read()
+// dataBase.update("Alisher", { age: 27, id: 999, tolov: true })
+// dataBase.read()
+// dataBase.findOne("Alisher1")
+// dataBase.delete("Xamidullo")
+// dataBase.read()
 

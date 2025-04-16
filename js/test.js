d9e71216c0e2af3524f781d3968df0b4b97c03cd
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-16 22:51:45 +0500

    up

--- js/test.js
+++ js/test.js
@@ -1,14 +1,98 @@
-// / TASK -7 Pulni Valyutaga Konvertatsiya Qilish
-// function changeConvert(sum) {
-//     if (sum < 0) {
-//         return `Noto'g'ri ma'lumot: Summa manfiy bo'lmasligi kerak!`;
+// // / TASK -1
+// function listContact() {
+//     console.log(`\n   Telefon Jadvallar\n`);
+//     for (let val of contacts) {
+//         console.log(`Ism: ${val.name}\tTel: ${val.tel}`);
 //     }
-//     if (isNaN(sum)) {
-//         return `Noto'g'ri ma'lumot: Kiritilgan format son bo'lishi kerak!`;
+// }
+// function addContact(names, phone) {
+//     if (!phone.includes("+998")) {
+//         return `\nNoto'g'ri format`
 //     }
-//     son = (sum / 12_500).toFixed(2)
-//     return `${son} USD`
+//     if (phone.length < 13) {
+//         return `\n13 belgidan kam`
+//     }
+//     for (let val of contacts) {
+//         if (val.name == names) {
+//             return `\n${names} foydalanuvchi bor!`
+//         }
+//     }
+//     for (let val of contacts) {
+//         if (val.tel == phone) {
+//             return `\n${phone} raqami bor!`
+//         }
+//     }
+//     let newContact = {
+//         name: names,
+//         tel:phone
+//     }
+//     contacts.push(newContact)
+//     return `\n${names} muvaffaqiyatli qo'shildi!\n`
+// }
+// function searchContact(numbers) {
+//     let info ="Topilgan Kontaktlar\n-------------------\n"
+//     for (let val of contacts) {
+//         if (val.tel.includes(numbers)) {
+//             info += "Ism: " + val.name + "\nTel: " + val.tel +"\n-------------------\n"
+//         }
+//     }
+//     console.log(info);
+// }
+// function deleteContact(deleteCont) {
+//     indexName = contacts.findIndex((val) => val.name == deleteCont)
+//     indexTel = contacts.findIndex((val) => val.tel == deleteCont)
+//     if (indexName !== -1) {
+//         contacts.splice(indexName, 1)
+//         return `${deleteCont} o'chirildi`
+//     }
+//     if (indexTel !== -1) {
+//         contacts.splice(indexTel, 1)
+//         return `${deleteCont} o'chirildi`
+//     }
     
 // }
-// const sum = +prompt("So'm kiriting!")
-// console.log(changeConvert(sum));
+// // -----------------------------------------------------------
+// let contacts = [
+//     { name: "Ali", tel: "+998901234567" },
+//     { name: "Vali", tel: "+998911112233" },
+//     { name: "Hasan", tel: "+998933335555" },
+//     { name: "Husan", tel: "+998935551199" },
+//     { name: "Aziz", tel: "+998977778888" },
+//     { name: "Shoxrux", tel: "+998935556677" },
+//     { name: "Diyor", tel: "+998909090909" },
+//     { name: "Sardor", tel: "+998915151515" },
+//     { name: "Murod", tel: "+998935577447" },
+//     { name: "Islom", tel: "+998999988877" }
+// ];
+// let ask = ""  //Shu yerdan komandalar beriladi
+// info = (`Mavjud komandalar:
+// - add: Yangi kontakt qo'shish
+// - search: Kontaktni qidirish
+// - delete: Kontaktni o'chirish
+// - list: Barcha kontaktlarni ko'rsatish
+// - help: Mavjud komandalarni ko'rsatish
+// - exit: Dasturdan chiqish`)
+// console.log(info);
+// if (ask == "add") {
+//     console.clear();
+//     listContact();
+//     console.log(addContact("Ali", "+998999988877"));
+//     console.log(addContact("Javohir","+998901234587"));
+//     listContact();
+// }else if (ask == "list") {
+//     console.clear();
+//     listContact();
+// }else if (ask == "search") {
+//     console.clear();
+//     console.log(searchContact("2"));
+// } else if (ask == "delete") {
+//     console.clear();
+//     console.log(deleteContact("Sardor"));
+//     listContact();
+// } else if (ask == "help") {
+//     console.clear();
+//     console.log(info);
+// } else if (ask == "exit") {
+//     console.clear();
+//     console.log("Dasturdan chiqildi");
+// }

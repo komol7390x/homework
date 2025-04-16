5520cb8cedc27f36c3820b9562865484256ed4b9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-16 18:58:46 +0500

    up

--- js/main.js
+++ js/main.js
@@ -32,7 +32,21 @@
 //         return matn;
 //     }
 //     return "Parol kuchli"
-    
+
 // }
 // console.log(checkPassword("SalDunaaa1"));
+// ----------------------------------------------------------------------------
+// TASK -2 Xarid Chekini Hisoblash
+// let price = +(prompt("Narx kiriting?"));
+// let count = +(prompt("Miqdori kiriting?"));
+// if (isNaN(price) ||isNaN(count)) {
+//     console.log("Son kiriting!\nNoto'g'ri ma'lumot!")
+// }
+// let total = price * count
+// console.log(`Umumiy narx: ${total} so'm\n`);
+// if (total >= 100_000) {
+//     console.log(`Chegirma: 10%\nOxirgi narx: ${total*0.9} so'm`);
+// } else {
+//     console.log(`Chegirma: yo'q\n`);
+// }
 // ----------------------------------------------------------------------------

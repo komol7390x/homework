688509ccbcb3e9e3663a1d6a7ec09b30e4e47db9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-16 20:02:42 +0500

    up

--- js/main.js
+++ js/main.js
@@ -74,3 +74,16 @@
 // }
 // console.log(changeName("Ahmad","Rahimov"));
 // ----------------------------------------------------------------------------
+// / TASK -5 Vaqtni Soat va Daqiqaga Ajratish
+// function changeTime(num) {
+//     if (num < 0) {
+//         return `Noto'g'ri ma'lumot: Daqiqa manfiy bo'lmasligi kerak!`
+//     }
+//     if (isNaN(num)) {
+//         return `Noto'g'ri ma'lumot: Kiritilgan format son bo'lishi kerak!`
+//     }
+//     return `${Math.floor(num/60)} soat ${num%60} daqiqa`
+// }
+// const num=+prompt("Son kiriting!")
+// console.log(changeTime(num));
+// ----------------------------------------------------------------------------

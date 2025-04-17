ad1432daa12147b8e3bae66a9ace51251235c547
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 22:51:23 +0500

    up

--- js/test.js
+++
@@ -1,7 +0,0 @@
-// function countTrue(array) {
-//     let res = array.filter((val) => val == true);
-//     let len = res.length;
-//     return `True: ${len} ta`
-// }
-// console.log(countTrue([true, false, false, true, false]));
-// console.log(countTrue([false, false, false, false]));

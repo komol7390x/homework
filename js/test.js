a21e4aa2b2e1aa941a53a2e272f5d971c3cbc424
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 21:46:35 +0500

    up

--- js/test.js
+++ js/test.js
@@ -1,4 +1,9 @@
-function func(array) {
-    res=
-}
-console.log(func([3,4]));
+// function func(array) {
+//     let sum=0
+//     for (let i = 0; i < array[1]; i++){
+//         sum+=array[0]
+//     }
+//     return sum
+// }
+// console.log(func([3, 4]));
+// console.log(func([2, 4]));

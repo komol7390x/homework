ca6380d15d8b7769b8aac311d1c50556e0011eab
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 22:46:26 +0500

    up

--- js/main.js
+++ js/main.js
@@ -90,5 +90,21 @@
 // console.log(spelling("happy"));
 // console.log(spelling("eagerly"));
 // -------------------------------------------------------------------------------
-
+// Task-9  Chat Room Status
+// function chatroomStatus(array = "") {
+//     let len = array.length
+//     if (len < 1) {
+//         return "no one online"
+//     } else if (typeof array==="string") {
+//         return `${array} online`
+//     } else if (len == 2) {
+//         return `${array[0]} and ${array[1]} online`
+//     } else {
+//         return `${array[0]}, ${array[1]} and ${len-2} more online`
+//     }
+// }
+// console.log(chatroomStatus([]));
+// console.log(chatroomStatus("paRIE_to"));
+// console.log(chatroomStatus(["s234f", "mailbox2"]));
+// console.log(chatroomStatus(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "sandwichmaker833", "violinist91"]));
 
---
+++ js/test.js
@@ -0,0 +1,16 @@
+// function chatroomStatus(array = "") {
+//     let len = array.length
+//     if (len < 1) {
+//         return "no one online"
+//     } else if (typeof array==="string") {
+//         return `${array} online`
+//     } else if (len == 2) {
+//         return `${array[0]} and ${array[1]} online`
+//     } else {
+//         return `${array[0]}, ${array[1]} and ${len-2} more online`
+//     }
+// }
+// console.log(chatroomStatus([]));
+// console.log(chatroomStatus("paRIE_to"));
+// console.log(chatroomStatus(["s234f", "mailbox2"]));
+// console.log(chatroomStatus(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "sandwichmaker833", "violinist91"]));

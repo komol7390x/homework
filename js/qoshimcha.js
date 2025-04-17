8ffc4f00319f8e253ab45917dfbae44165fc204b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 23:12:42 +0500

    up

--- js/qoshimcha.js
+++ js/qoshimcha.js
@@ -17,4 +17,16 @@
 // }
 // console.log(findPrimes(20));
 // --------------------------------------------------------------------------------------
+// Task-2 
+// function multiply(array) {
+//     return function (son) {
+//         res = array.map((val) => val * son)
+//         return res
+//     }
+// }
+// console.log(multiply([1, 2, 3])(2));
+// console.log(multiply([4, 6, 5])(10));
+// console.log(multiply([1, 2, 3])(0));
+// --------------------------------------------------------------------------------------
+
 
--- js/test.js
+++ js/test.js
@@ -1,16 +1,10 @@
 // Task-2 
-// function multiply(array, son) {
-//     return function () {
-//         console.log('array:', array);  // array qiymatini tekshiring
-//         console.log('son:', son);      // son qiymatini tekshiring
-//         return array.map((val) => {
-//             console.log(`val: ${val}, val * son: ${val * son}`);
-//             return val * son;
-//         });
+// function multiply(array) {
+//     return function (son) {
+//         res = array.map((val) => val * son)
+//         return res
 //     }
 // }
-// console.log(multiply([1, 2, 3])(2));  // [2, 4, 6]
-console.log(1111);
-
-// console.log(multiply([4, 6, 5],10));
-// console.log(multiply([1, 2, 3], 2));
+// console.log(multiply([1, 2, 3])(2));
+// console.log(multiply([4, 6, 5])(10));
+// console.log(multiply([1, 2, 3])(0));

d3f8eb0e953e7bc781ad787c7b239b8d7728e6f6
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 23:17:03 +0500

    up

--- js/qoshimcha.js
+++ js/qoshimcha.js
@@ -28,5 +28,16 @@
 // console.log(multiply([4, 6, 5])(10));
 // console.log(multiply([1, 2, 3])(0));
 // --------------------------------------------------------------------------------------
+// Task-3
+// function findNumber(num='') {
+//     let len = num.length
+//     for (let i = 0; i <= len; i++){
+//         if (!num.includes(i)) {
+//             return i
+//         }
+//     }
+// }
+// console.log(findNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+// console.log(findNumber([3, 0, 1]));
 
 
--- js/test.js
+++ js/test.js
@@ -1,10 +1,10 @@
-// Task-2 
-// function multiply(array) {
-//     return function (son) {
-//         res = array.map((val) => val * son)
-//         return res
+// function findNumber(num='') {
+//     let len = num.length
+//     for (let i = 0; i <= len; i++){
+//         if (!num.includes(i)) {
+//             return i
+//         }
 //     }
 // }
-// console.log(multiply([1, 2, 3])(2));
-// console.log(multiply([4, 6, 5])(10));
-// console.log(multiply([1, 2, 3])(0));
+// console.log(findNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+// console.log(findNumber([3, 0, 1]));

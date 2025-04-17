0c562f400ea5ddafc96f7a3566235d8d037ff37b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 23:07:50 +0500

    up

---
+++ js/qoshimcha.js
@@ -0,0 +1,20 @@
+// Task - 1 Function yarating u array qabul qilsin va tub sonlarni qaytarsin! >
+// function isPrime(num) {
+//     if (num <= 1) return false;
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) return false;
+//     }
+//     return true;
+// }
+// function findPrimes(n) {
+//     let tubArray = [];
+//     for (let i = 2; i <= n; i++) {
+//         if (isPrime(i)) {
+//             tubArray.push(i);
+//         }
+//     }
+//     return tubArray;
+// }
+// console.log(findPrimes(20));
+// --------------------------------------------------------------------------------------
+
---
+++ js/test.js
@@ -0,0 +1,16 @@
+// Task-2 
+// function multiply(array, son) {
+//     return function () {
+//         console.log('array:', array);  // array qiymatini tekshiring
+//         console.log('son:', son);      // son qiymatini tekshiring
+//         return array.map((val) => {
+//             console.log(`val: ${val}, val * son: ${val * son}`);
+//             return val * son;
+//         });
+//     }
+// }
+// console.log(multiply([1, 2, 3])(2));  // [2, 4, 6]
+console.log(1111);
+
+// console.log(multiply([4, 6, 5],10));
+// console.log(multiply([1, 2, 3], 2));

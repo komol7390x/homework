8ee6702d7c6094cabfda23b73764689a4010d766
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-17 21:21:20 +0500

    up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// Task-1
+// function numberSplit(num) {
+//     const a = Math.floor(num / 2);
+//         if (num % 2 != 0) {
+//             return [a, a+1];
+//         } else {
+//             return [a, a];
+//         }
+// }
+// console.log(numberSplit(-9));
+// console.log(numberSplit(-8));
+// console.log(numberSplit(9));
+// console.log(numberSplit(8));
+// -----------------------------------------------
 
---
+++ js/test.js
@@ -0,0 +1,13 @@
+// Task-1
+// function numberSplit(num) {
+//     const a = Math.floor(num / 2);
+//         if (num % 2 != 0) {
+//             return [a, a+1];
+//         } else {
+//             return [a, a];
+//         }
+// }
+// console.log(numberSplit(-9));
+// console.log(numberSplit(-8));
+// console.log(numberSplit(9));
+// console.log(numberSplit(8));

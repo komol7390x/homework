54e980093c9fa18d9c64b40d1da1dfdeb46a403b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-19 18:02:27 +0500

    up

--- js/main.js
+++ js/main.js
@@ -342,3 +342,39 @@
 //     ["Z", 23],
 // ])
 // console.log(sumAlpha(scores,"Muhammadali"));
+// ------------------------------------------------------------------------------------
+// Bonus
+// function determineWinnerOfFight(map) {
+//     let matn = [...map]
+//     let wife = 0;
+//     let husband = 0;
+//     for (let [key, val] of matn) {
+//         if (val.me > val.spouse) {
+//             husband += 1
+//         }
+//         else if (val.me < val.spouse) {
+//             wife += 1
+//         }
+//     }
+//     if (wife > husband) {
+//         return `Xotin yutdi| ${wife}:${husband}`
+//     } else if (wife < husband) {
+//         return `Er yutdi| ${wife}:${husband}`
+//     } else {
+//         return `Durang| ${wife}:${husband}`
+//     }
+// }
+
+// let map1 = new Map([
+//     ["round1", { me: 10, spouse: 5, }],
+//     ["round2", { me: 5, spouse: 20, }],
+//     ["round3", { me: 10, spouse: 10, }],
+
+// ])
+
+// let map2 = new Map([
+//     ["round1", { me: 40, spouse: 5, }],
+//     ["round2", { me: 11, spouse: 10, }],
+//     ["round3", { me: 9, spouse: 10, }],
+// ])
+// console.log(determineWinnerOfFight(map2));

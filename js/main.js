b8e5667e6511c1a2208d5de8abaf5be60c4b9e0e
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-19 13:06:52 +0500

    up

--- js/main.js
+++ js/main.js
@@ -280,3 +280,15 @@
 // console.log(afterNYears(people,3));
 // ------------------------------------------------------------------------------------
 // Task-9
+// function removeEntry(user, name) {
+//     let res = {...user};
+//     delete user[name]
+//     return user
+// }
+
+// let object = new Map([
+//     ["piano",300],
+//     ["tv", 100],
+//     ["skate",50]
+// ])
+// console.log(removeEntry({ piano: 300, tv: 100, skate: 50 }, "skate"));
--- js/test.js
+++ js/test.js
@@ -1,16 +1,12 @@
-// function afterNYears(user, num) {
-//     user.forEach((val, key) => {
-//         user.set(key,val+num)
-//     })
-//     for (let [key, value] of user) {
-//         console.log(`Ism: ${key}\nAvvalgi yosh: ${value - num} yosh\nHozirgi yosh: ${value} yosh\nQo'shilgan yil: ${num} yil\n---------------------`);
-//     }
+// function removeEntry(user, name) {
+//     let res = {...user};
+//     delete user[name]
+//     return user
 // }
-// let people = new Map([
-//     ["Joel" , 32],
-//     ["Fred" , 44],
-//     ["Reginald" , 65],
-//     ["Susan" , 33],
-//     ["Julian" , 13] 
+
+// let object = new Map([
+//     ["piano",300],
+//     ["tv", 100],
+//     ["skate",50]
 // ])
-// console.log(afterNYears(people,3));
+// console.log(removeEntry({ piano: 300, tv: 100, skate: 50 }, "skate"));

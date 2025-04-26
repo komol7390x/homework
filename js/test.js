07a1fe73aecfacfcb7a82d60303a9ed30651b171
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-26 17:53:09 +0500

    up

--- js/test.js
+++ js/test.js
@@ -1,35 +1,13 @@
-// function determineWinnerOfFight(map) {
-//     let matn = [...map]
-//     let wife = 0;
-//     let husband = 0;
-//     for (let [key, val] of matn) {
-//         if (val.me > val.spouse) {
-//             husband += 1
-//         }
-//         else if (val.me < val.spouse) {
-//             wife += 1
-//         }
-//     }
-//     if (wife > husband) {
-//         return `Xotin yutdi| ${wife}:${husband}`
-//     } else if (wife < husband) {
-//         return `Er yutdi| ${wife}:${husband}`
-//     } else {
-//         return `Durang| ${wife}:${husband}`
-//     }
-// }
+// Normal rejimda
+function myFunction() {
+    undeclaredVar = 10; // globalga chiqarib yuboriladi
+}
+myFunction();
+console.log(undeclaredVar); // 10
 
-// let map1 = new Map([
-//     ["round1", { me: 10, spouse: 5, }],
-//     ["round2", { me: 5, spouse: 20, }],
-//     ["round3", { me: 10, spouse: 10, }],
-
-// ])
-
-// let map2 = new Map([
-//     ["round1", { me: 40, spouse: 5, }],
-//     ["round2", { me: 11, spouse: 10, }],
-//     ["round3", { me: 9, spouse: 10, }],
-// ])
-// console.log(determineWinnerOfFight(map2));
-
+// 'use strict' rejimida
+'use strict';
+function myFunctionStrict() {
+    undeclaredVar = 10; // ReferenceError: undeclaredVar is not defined
+}
+myFunctionStrict();
